6d592acc6cf5ff71c85d63d215fa9e7c20489531
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-09-20 10:19:59 -0400

    catch-up commit

--- build_docker.sh
+++
@@ -1,6 +0,0 @@
-# run this command in the geospatial_extend directory
-# update the version tag before running this!
-docker build . -t melaniekamm/geospatial_extend:latest -t melaniekamm/geospatial_extend:v1.2
-
-# push latest and vXXX tagged containers to dockerhub
-docker push melaniekamm/geospatial_extend --all-tags
